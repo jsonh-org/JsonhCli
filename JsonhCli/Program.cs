277185ed8e49220c24c0e4ce77d816ede08e1dab
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-05-24 01:42:53 +0100

    Use raw System.CommandLine not DragonFruit

--- JsonhCli/Program.cs
+++ JsonhCli/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Nodes;
+using System.CommandLine;
 using JsonhCs;
 using ResultZero;
 
@@ -8,54 +9,81 @@ namespace JsonhCli;
 /// The JSONH Command Line Interface.
 /// </summary>
 public static class Program {
-    /// <summary>
-    /// The JSONH Command Line Interface.
-    /// </summary>
-    /// <param name="InputPath">The path of the JSONH file to input.</param>
-    /// <param name="Input">The JSONH string to input.</param>
-    /// <param name="OutputPath">The path of the JSON file to output. If null, logs the output.</param>
-    /// <param name="Pretty">Whether to indent the outputted JSON.</param>
-    public static void Main(string? InputPath = null, string? Input = null, string? OutputPath = null, bool Pretty = false) {
-        // JSONH file to JSON file
-        if (InputPath is not null || Input is not null) {
-            // Ensure unambiguous input
-            if (InputPath is not null && Input is not null) {
-                Console.WriteLine("Cannot pass both an input file and an input string.");
-                return;
-            }
-            // Read input path
-            if (InputPath is not null) {
-                try {
-                    Input = File.ReadAllText(InputPath);
+    public static int Main(string[] Args) {
+        Option<string?> InputPathOption = new(
+            name: "--input-path",
+            description: "The path of the JSONH file to input"
+        );
+        Option<string?> InputOption = new(
+            name: "--input",
+                description: "The JSONH string to input"
+        );
+        Option<string?> OutputPathOption = new(
+            name: "--output-path",
+                description: "The path of the JSON file to output. If null, logs the output"
+        );
+        Option<bool> PrettyOption = new(
+            name: "--pretty",
+            description: "Whether to indent the outputted JSON"
+        );
+
+        RootCommand RootCommand = new("The JSONH Command Line Interface") {
+            InputPathOption,
+            InputOption,
+            OutputPathOption,
+            PrettyOption,
+        };
+
+        RootCommand.SetHandler(
+            (string? InputPath, string? Input, string? OutputPath, bool Pretty) => {
+                // JSONH file to JSON file
+                if (InputPath is not null || Input is not null) {
+                    // Ensure unambiguous input
+                    if (InputPath is not null && Input is not null) {
+                        Console.WriteLine("Cannot pass both an input file and an input string.");
+                        return;
+                    }
+                    // Read input path
+                    if (InputPath is not null) {
+                        try {
+                            Input = File.ReadAllText(InputPath);
+                        }
+                        catch (Exception) {
+                            Console.WriteLine("The input file is invalid.");
+                            return;
+                        }
+                    }
+                    // Parse JSONH
+                    if (JsonhReader.ParseNode(Input!).TryGetError(out Error Error, out JsonNode? Node)) {
+                        Console.WriteLine($"Error parsing JSONH file: \"{Error.Message}\"");
+                        return;
+                    }
+                    // Convert to JSON
+                    string Json = Node.ToJsonString(Pretty ? JsonhReader.PrettyJson : JsonhReader.MiniJson);
+                    // Write output
+                    if (OutputPath is not null) {
+                        // Validate output path
+                        if (OutputPath is null || !Directory.Exists(Path.GetDirectoryName(OutputPath))) {
+                            Console.WriteLine($"The directory of the output file does not exist: \"{OutputPath}\"");
+                            return;
+                        }
+                        File.WriteAllText(OutputPath, Json);
+                    }
+                    // Log output
+                    else {
+                        Console.WriteLine(Json.ReplaceLineEndings());
+                    }
                 }
-                catch (Exception) {
-                    Console.WriteLine("The input file is invalid.");
-                    return;
+                else {
+                    Console.WriteLine("Invalid arguments.");
                 }
-            }
-            // Parse JSONH
-            if (JsonhReader.ParseNode(Input!).TryGetError(out Error Error, out JsonNode? Node)) {
-                Console.WriteLine($"Error parsing JSONH file: \"{Error.Message}\"");
-                return;
-            }
-            // Convert to JSON
-            string Json = Node.ToJsonString(Pretty ? JsonhReader.PrettyJson : JsonhReader.MiniJson);
-            // Write output
-            if (OutputPath is not null) {
-                // Validate output path
-                if (OutputPath is null || !Directory.Exists(Path.GetDirectoryName(OutputPath))) {
-                    Console.WriteLine($"The directory of the output file does not exist: \"{OutputPath}\"");
-                    return;
-                }
-                File.WriteAllText(OutputPath, Json);
-            }
-            // Log output
-            else {
-                Console.WriteLine(Json.ReplaceLineEndings());
-            }
-        }
-        else {
-            Console.WriteLine("Invalid arguments.");
-        }
+            },
+            InputPathOption,
+            InputOption,
+            OutputPathOption,
+            PrettyOption
+        );
+
+        return RootCommand.Invoke(Args);
     }
 }
